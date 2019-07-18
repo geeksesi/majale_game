a3d929bd7c66990724d8446ffb8e90a1dd4feadd
mohammad javad ghasemy <geeksesi@gmail.com>
2019-07-18 09:00:04 +0430

    start v0.3.0
    make mongoDB module of everything we need to store to know user

---
+++ server/db/mongo/modules.js
@@ -0,0 +1,140 @@
+const mongoos = require('mongoose');
+
+
+const actio_historySchema = mongoos.Schema({
+    // _id: mongoos.Schema.Types.ObjectId,
+    user_id: {
+        type: mongoos.Schema.Types.ObjectId,
+        require: true,
+    },
+    /**
+     * - payment
+     * - hint
+     * - finish_lvl
+     */
+    type: {
+        type: String,
+        require: true,
+    },
+    value: {
+        type: Number,
+        require: true,
+    },
+    /**
+     * - Xx lvl_point
+     * - payment on market offer
+     * - hint:(word_id)
+     * - payment when play          \
+     *                                # when user did payment
+     * - payment from market page   /
+     */
+    description: {
+        type: String,
+        require: true,
+    },
+    timestamp: {
+        type: Number,
+        require: true,
+    },
+});
+
+
+const play_time_historySchema = mongoos.Schema({
+    // _id: mongoos.Schema.Types.ObjectId,
+    user_id: {
+        type: mongoos.Schema.Types.ObjectId,
+        require: true,
+    },
+    online_timestamp: {
+        type: Number,
+        require: true,
+    },
+    offline_timestamp: {
+        type: Number,
+        require: false,
+    },
+    // how much take to full load game
+    load_time: {
+        type: Number,
+        require: false,
+    },
+});
+
+
+const remember_wordSchema = mongoos.Schema({
+    // _id: mongoos.Schema.Types.ObjectId,
+    user_id: {
+        type: mongoos.Schema.Types.ObjectId,
+        require: true,
+    },
+    word_id: {
+        type: Number,
+        require: true,
+    },
+    try_count: {
+        type: Number,
+        require: true,
+    },
+    /**
+     * - wait 
+     * - answered
+     * - skip 
+     * - expire
+     */
+    status : {
+        type : String,
+        require : true,
+    },
+    timestamp: {
+        type: Number,
+        require: true,
+    },
+    /**
+     * will store a json of time the user did this word
+     */
+    try_date: {
+        type: Number,
+        require: true,
+    },
+});
+
+const userSchema = mongoos.Schema({
+    // _id: mongoos.Schema.Types.ObjectId,
+
+    rubicka_id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    credit: {
+        type: Number,
+        require: false,
+        default: 25
+    },
+    play_time: {
+        type: Number,
+        require: false,
+        default: 0,
+    },
+    profile: {
+        type: String,
+        require: false,
+    },
+    timestamp: {
+        type: Number,
+        require: true,
+    },
+
+});
+
+const user = mongoos.model("user", userSchema);
+const remember_word = mongoos.model("remember_word", remember_wordSchema);
+const actio_history = mongoos.model("actio_history", actio_historySchema);
+const play_time_history = mongoos.model("play_time_history", play_time_historySchema);
+
+module.exports = {
+    user: user,
+    remember_word : remember_word,
+    actio_history : actio_history,
+    play_time_history : play_time_history,
+};
