7feaa0d10006406eb4d39f397fe67d0bcd0e8efe
mohammad javad ghasemy <geeksesi@gmail.com>
2019-07-20 03:09:55 +0430

    remove useless things

--- client/src/server.js
+++ client/src/server.js
@@ -20,14 +20,7 @@ function get_season(language_id) {
 function user_data() {
     return new Promise((res, rej) => {
         res(user);
-        // res({
-        //     "credit": 25,
-        //     "play_time": 0,
-        //     "_id": "5d3188f1e120931f2ef0cfa0",
-        //     "rubicka_id": "1",
-        //     "timestamp": 1563527409,
-        //     "__v": 0
-        // });
+
     })
 }
 
@@ -36,34 +29,6 @@ function get_word(season_id) {
         console.log(word_list[season_id])
         res(word_list[season_id]);
     })
-    // return new Promise((res, rej) => {
-    //     // console.log(word_list[season_id])
-    //     res([{
-    //             "id": 312448,
-    //             "parent_id": 11,
-    //             "language_id": 2,
-    //             "season_sort": 1,
-    //             "word": "Body",
-    //             "status": "1",
-    //             "answer": {
-    //                 "word": "بدن",
-    //                 "language_id": 1
-    //             }
-    //         },
-    //         {
-    //             "id": 312601,
-    //             "parent_id": 41,
-    //             "language_id": 2,
-    //             "season_sort": 2,
-    //             "word": "Foot",
-    //             "status": "1",
-    //             "answer": {
-    //                 "word": "پا",
-    //                 "language_id": 1
-    //             }
-    //         }
-    //     ]);
-    // })
 }
 
 
@@ -93,15 +58,12 @@ async function init() {
             })
         });
     });
-    // console.log(await loader)
     await socket.emit("season_list", 2, async res => {
         season_list[2] = res.data;
         await res.data.forEach(async s => {
             await socket.emit("word_season", s.id, 1, async words => {
-                // console.log(words)
                 word_list[s.id] = words.data;
                 await loader++;
-                // console.log(loader)
             })
         });
     });
@@ -114,20 +76,10 @@ async function init() {
             })
         });
     });
-    // setTimeout(() => console.log(loader), 3000)
 
     await socket.emit("user_login", 1, res => {
         user = res.data;
-        // console.log(user);
     });
-    // setTimeout(async ()=> console.log(await get_season(2)), 2000)
-    // const seasons = await get_season(2);
-    // seasons.forEach(element => {
-
-    // });
-    // setTimeout(async () => console.log(word_list), 2000)
-    // let word = await get_word(1);
-    // console.log(word)
 
 }
 
