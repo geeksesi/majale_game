86f16cd3b7616ea5c50291f4ecb235ec3239dd69
mohammad javad ghasemy <geeksesi@gmail.com>
2019-07-29 10:35:47 +0430

    update gitignore and fix db password

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { my_io } = require('./server/server');
 const mongoos = require('mongoose');
 
 // mongoos.connect('mongodb://127.0.0.1:27017/majale', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
-mongoos.connect('mongodb://root:'+encodeURIComponent('X3vm3eCq9Lqmk4Tf10iWaICj')+'@s7.liara.ir:32250/majale?authSource=admin', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+mongoos.connect('mongodb://root:'+encodeURIComponent('pKm7tTDd9SgSJXX7VxLTAmrY')+'@s7.liara.ir:33330/majale?authSource=admin', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
 
 app.use('/', express.static('public_html'))
 
