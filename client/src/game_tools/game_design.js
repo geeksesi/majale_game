a436a5600fc65ee29735f5b057a9c4ca77ec0060
mohammad javad ghasemy <geeksesi@gmail.com>
2019-08-07 02:39:40 +0430

    format document and a fail try to get some data

--- client/src/game_tools/game_design.js
+++ client/src/game_tools/game_design.js
@@ -1,5 +1,5 @@
-async function make_road(words, finished_word, remembers, language_id,cb) {
-    
+async function make_road(words, finished_word, remembers, language_id, cb) {
+
     let new_words = [];
     let remembers_word = [];
     let check_finish = false;
@@ -20,17 +20,17 @@ async function make_road(words, finished_word, remembers, language_id,cb) {
             }
         })
     })
-    setTimeout(()=>{
+    setTimeout(() => {
         check_finish = true;
     }, 2000)
     let wait_for_him = setInterval(async() => {
         if (check_finish) {
             clearInterval(wait_for_him);
-                // console.log(await make_final_road(new_words, remembers_word))
-                let road = await make_final_road(new_words, remembers_word)
-                setTimeout(()=>{
-                    cb(road)
-                },500)
+            // console.log(await make_final_road(new_words, remembers_word))
+            let road = await make_final_road(new_words, remembers_word)
+            setTimeout(() => {
+                cb(road)
+            }, 500)
         }
     }, 500)
 
@@ -59,4 +59,60 @@ function make_final_road(new_words, remembers) {
     })
 }
 
-export { make_road };
+function user_path(words, last_finished_word_key, remembers, language_id, season, cb) {
+    let output = {
+        remember_length : remembers.length,
+        season_id : 0,
+        season_name : "",
+        season_length : 0,
+        season_finished_length : 0,
+    }
+
+    Object.keys(words).forEach(key => {
+        words[key].forEach(word => {
+            if (word.id === last_finished_word_key) {
+                season_id = key;
+                season_length = words[key].length;
+                if (word.season_sort < words[key].length) {
+                    output.season_id = key;
+                    output.season_length = words[key].length;
+                    output.season_finished_length = word.season_sort;
+                    console.log(output.season_id)
+                } else {
+                    if (typeof words[key + 1] !== 'undefined') {
+                        output.season_id = key+1;
+                        output.season_length = words[key+1].length;
+                        output.season_finished_length = 1;
+                        console.log(output.season_id)
+                    } else {
+                        cb({ ok: false })
+                    }
+                }
+            }
+        });
+    });
+
+    let wait_for_season_id = setInterval(() => {
+        if (output.season_id !== 0) {
+            clearInterval(wait_for_season_id);
+            season_info(output.season_id, season, language_id, name => {
+                output.season_name = name;
+                setTimeout(() => {
+                    cb(output);
+                }, 200)
+            })
+        }
+    }, 500);
+
+}
+
+
+function season_info(season_id, season, language_id, cb) {
+    season[language_id].forEach(season => {
+        if (season.id === season_id) {
+            cb(season.name)
+        }
+    })
+}
+
+export { make_road, user_path };
