5f0ab3d3efa6755a5fd96608756c1e27fc493fea
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-08-19 04:01:33 +0430

    connection lost ui events

---
+++ client/src/game_tools/offline.js
@@ -0,0 +1,9 @@
+function offline() {
+    document.getElementById('WIFILOST').className = "WIFIANIM";
+}
+
+function online() {
+    document.getElementById('WIFILOST').className = "";
+}
+
+export { offline, online }
