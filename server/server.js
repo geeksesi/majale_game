cca63235fd4e24de81669110b511e9658603fcc7
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-08-21 09:35:04 +0430

    remember work fixed

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ module.exports.my_io = function (server) {
                     })
                 });
 
-                await remember_word.find({ user_id: socket._id, status: 'wait', timestamp: { $gt: socket.online_timestamp + 64800 } }, (err, remembers) => {
+                await remember_word.find({ user_id: socket._id, status: 'wait', timestamp: { $lt: socket.online_timestamp - 64800 } }, (err, remembers) => {
                     remembers.forEach(remember => {
                         export_object.remembers_id.push(remember.word_id)
                     })
