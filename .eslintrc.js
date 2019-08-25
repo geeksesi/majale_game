898c190ee06f56cb8efecf3163fac7d18e7ac26c
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-08-25 13:48:21 +0430

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,6 @@ module.exports = {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
         "indent": [
             "error",
