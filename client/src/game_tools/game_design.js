4a090033d8123cdac7be191662452d0f6cc35898
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-04 08:37:47 +0430

    make test path

--- client/src/game_tools/game_design.js
+++ client/src/game_tools/game_design.js
@@ -1,14 +1,27 @@
 /* eslint-disable no-async-promise-executor */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import { season_finish_data, get_finished_season, splice_word, get_finished_word } from './../server';
+import {
+	set_user_level,
+	get_user_level,
+	splice_word,
+	get_finished_word,
+	store_finished_season,
+	store_finished_word
+} from './../server';
+
 
 function chance_to_add(cb) {
 	const chances = [1, 0, 0, 0, 1, 1, 0];
 	cb(chances[Math.floor(Math.random() * chances.length)]);
 }
+function chance_to_test(cb) {
+	const chances = [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0];
+	cb(chances[Math.floor(Math.random() * chances.length)]);
+}
 
-async function make_road(words, finished_word, remembers, language_id, cb) {
+
+async function make_road(level, words, finished_word, remembers, language_id, cb) {
 	let new_words = [];
 	let remembers_word = [];
 	let check_finish = false;
@@ -20,7 +33,7 @@ async function make_road(words, finished_word, remembers, language_id, cb) {
 			}
 		}
 		if (typeof finished_word[word.id] === 'undefined' && word.language_id === language_id) {
-			if (typeof word.answer !== 'undefined' && word.answer.word.length < 12)
+			if (typeof word.answer !== 'undefined' && word.answer.word.length < 12 && word.status == level)
 				chance_to_add(yes_or_not => {
 					if (yes_or_not)
 						new_words.push(word);
@@ -91,4 +104,59 @@ function user_level(cb) {
 	});
 }
 
-export { make_road, user_level };
+
+function make_test(level, word_list, language_id, cb) {
+	let new_words = [];
+	let check_finish = false;
+	word_list.every(async word => {
+		if (new_words.length > 9) {
+			check_finish = true;
+			return true;
+
+		}
+		if (word.language_id === language_id) {
+			if (typeof word.answer !== 'undefined' && word.answer.word.length < 12 && word.status == level)
+				chance_to_test(yes_or_not => {
+					if (yes_or_not)
+						new_words.push(word);
+				});
+			else
+				splice_word(word.season_id, word.id);
+		} else {
+			// Do Nothing
+		}
+	});
+
+	setTimeout(() => {
+		check_finish = true;
+	}, 2000);
+
+	let wait_for_him = setInterval(async () => {
+		if (check_finish) {
+			clearInterval(wait_for_him);
+			if (new_words.length < 10)
+				make_test(level, word_list, language_id, res => {
+					cb(res);
+				});
+			else
+				setTimeout(() => {
+					cb(new_words);
+				}, 500);
+		}
+	}, 500);
+}
+
+
+async function lvlup_user() {
+	const user_level = await get_user_level();
+	if (user_level > 3) {
+		console.log('all of level has finished');
+	} else {
+		console.log('level growed ');
+		set_user_level(user_level + 1);
+		store_finished_season([]);
+		store_finished_word({});
+	}
+}
+
+export { make_road, user_level, make_test, lvlup_user };
