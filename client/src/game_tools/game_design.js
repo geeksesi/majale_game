b093561c5c368b68400b0a88ca9561c6efc043ea
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-04 05:50:15 +0430

    user level selection now is better

--- client/src/game_tools/game_design.js
+++ client/src/game_tools/game_design.js
@@ -4,7 +4,7 @@
 import { season_finish_data, get_finished_season, splice_word, get_finished_word } from './../server';
 
 function chance_to_add(cb) {
-	const chances = [1, 1, 0];
+	const chances = [1, 0, 0, 0, 1, 1, 0];
 	cb(chances[Math.floor(Math.random() * chances.length)]);
 }
 
@@ -12,36 +12,31 @@ async function make_road(words, finished_word, remembers, language_id, cb) {
 	let new_words = [];
 	let remembers_word = [];
 	let check_finish = false;
-	await Object.keys(words).forEach(word_key => {
-		chance_to_add(true_or_false => {
-			if (true_or_false)
-				words[word_key].every(async word => {
-					if (new_words.length > 7) {
-						if (remembers_word.length === remembers.length || remembers_word.length > 9) {
-							check_finish = true;
-							return true;
-						}
-					}
-					if (typeof finished_word[word.id] === 'undefined' && word.language_id === language_id) {
-						if (typeof word.answer !== 'undefined' && word.answer.word.length < 12)
-							chance_to_add(yes_or_not => {
-								if (yes_or_not)
-									new_words.push(word);
-							});
-						else
-							splice_word(word.season_id, word.id);
-					} else {
-						if (remembers.includes(word.id) && word.language_id === language_id) {
-							if (typeof word.answer !== 'undefined' && word.answer.word.length < 12)
-								await remembers_word.push(word);
-							else
-								splice_word(word.season_id, word.id);
-						}
-					}
+	words.every(async word => {
+		if (new_words.length > 7) {
+			if (remembers_word.length === remembers.length || remembers_word.length > 9) {
+				check_finish = true;
+				return true;
+			}
+		}
+		if (typeof finished_word[word.id] === 'undefined' && word.language_id === language_id) {
+			if (typeof word.answer !== 'undefined' && word.answer.word.length < 12)
+				chance_to_add(yes_or_not => {
+					if (yes_or_not)
+						new_words.push(word);
 				});
-		});
-
+			else
+				splice_word(word.season_id, word.id);
+		} else {
+			if (remembers.includes(word.id) && word.language_id === language_id) {
+				if (typeof word.answer !== 'undefined' && word.answer.word.length < 12)
+					await remembers_word.push(word);
+				else
+					splice_word(word.season_id, word.id);
+			}
+		}
 	});
+
 	setTimeout(() => {
 		check_finish = true;
 	}, 2000);
@@ -93,66 +88,6 @@ function user_level(cb) {
 			length: length,
 			completed: completed,
 		});
-	});
-}
-function user_level_old(cb) {
-	get_finished_season(async seasons_id => {
-		if (seasons_id.length < 1) {
-			let finish = false;
-			let completed = 0;
-			let season_data = await season_finish_data(1);
-			for (let i = 0; i < season_data.length; i++) {
-				if (typeof season_data[i].status === 'undefined') {
-					finish = true;
-					break;
-				} else {
-					completed++;
-				}
-				if (typeof season_data[i + 1] === 'undefined') {
-					finish = true;
-				}
-			}
-			const wait_interval = setInterval(() => {
-				if (finish) {
-					clearInterval(wait_interval);
-					cb({
-						ok: true,
-						season_id: 1,
-						length: season_data.length,
-						completed: completed,
-					});
-				}
-			}, 200);
-		}
-		else {
-			let season_id = await seasons_id[seasons_id.length - 1] + 1;
-			let finish = false;
-			let completed = 0;
-			let season_data = await season_finish_data(season_id);
-			for (let i = 0; i < season_data.length; i++) {
-				if (typeof season_data[i].status === 'undefined') {
-					finish = true;
-					break;
-				} else {
-					completed++;
-				}
-				if (typeof season_data[i + 1] === 'undefined') {
-					finish = true;
-				}
-			}
-			const wait_interval = setInterval(() => {
-				if (finish) {
-					clearInterval(wait_interval);
-					cb({
-						ok: true,
-						season_id: season_id,
-						length: season_data.length,
-						completed: completed,
-					});
-				}
-			}, 200);
-
-		}
 	});
 }
 
